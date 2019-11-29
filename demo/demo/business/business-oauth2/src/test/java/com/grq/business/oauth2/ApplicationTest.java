200670b84dd895f30dc1a507a3e0ce29c2baa12a
grq <18300466806@163.com>
2019-11-29 13:04:35 +0800

    UPDATE

---
+++ demo/demo/business/business-oauth2/src/test/java/com/grq/business/oauth2/ApplicationTest.java
@@ -0,0 +1,57 @@
+package com.grq.business.oauth2;
+
+import okhttp3.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+import java.util.Objects;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class ApplicationTest {
+
+    @Test
+    public void testGet(){
+        String url = "https://www.baidu.com";
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        Call call = client.newCall(request);
+        try {
+            Response response = call.execute();
+            System.out.println(Objects.requireNonNull(response.body()).string());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testPost(){
+        String url = "http://localhost:9001/oauth/token";
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body = new FormBody.Builder()
+                .add("username", "admin")
+                .add("password", "123456")
+                .add("grant_type", "password")
+                .add("client_id", "client")
+                .add("client_secret", "secret")
+                .build();
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        Call call = client.newCall(request);
+        try {
+            Response response = call.execute();
+            System.out.println(response.body().string());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
